c34d248c91769e5f7646a5d6b2b2e3c8098c63e1
Kapunahele Wong <kapunahelewong@gmail.com>
2023-03-13 14:35:20 -0700

    learn how to set up 404

---
+++ src/routes/404.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export const NotFound = component$(() => {
+  return (
+    <header>
+      <h1>This is my 404 Component from 404.tsx</h1>
+    </header>
+  );
+});
